ad8d48172ce73e92845d392c9624425020b48b26
WanjiruMaina15 <wanjjirumaina@gmail.com>
2025-09-05 18:07:05 +0300

    WK6

---
+++ answers.sql
@@ -0,0 +1,35 @@
+### **Question 1 🧑‍💼**  
+**Write an SQL query** to get the **firstName**, **lastName**, **email**, and **officeCode** of all employees.  
+Use an **INNER JOIN** to combine the **employees** table with the **offices** table using the **officeCode** column.
+
+USE salesdB;
+
+-- -- firstName**, **lastName**, **email**, and **officeCode** of all employees.  
+
+SELECT employees.firstName, employees.lastName, employees.email, offices.officeCode
+FROM employees
+INNER JOIN offices
+ON employees.officeCode = offices.officeCode;
+
+
+### **Question 2 🛍️**  
+**Write an SQL query** to get the **productName**, **productVendor**, and **productLine** from the **products** table.  
+Use a **LEFT JOIN** to combine the **products** table with the **productlines** table using the **productLine** column.
+
+-- -- productName**, **productVendor**, and **productLine** from the **products** table.  
+SELECT p.productName, p.productVendor, pl.productLine
+FROM products p
+LEFT JOIN productlines pl
+ON p.productLine = pl.productLine;
+
+### **Question 3 📦**  
+**Write an SQL query** to retrieve the **orderDate**, **shippedDate**, **status**, and **customerNumber** for the first 10 orders.  
+Use a **RIGHT JOIN** to combine the **customers** table with the **orders** table using the **customerNumber** column.
+
+---
+
+SELECT o.orderDate, o.shippedDate, o.status, o.customerNumber
+FROM customers c
+RIGHT JOIN orders o
+ON c.customerNumber = o.customerNumber
+LIMIT 10;
